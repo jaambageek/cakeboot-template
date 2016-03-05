eb69a2bd23e1014b4a5cdbeaf30c2c5ecb92301e
Stephen Valliere <steve@vallierebrothers.com>
2016-03-05 17:50:38 -0500

    Updated artifacts text and image only
    All formatting handled outside the artifact element. Artifacts only
    return the content. Simplified the edit screen for artifacts.

--- plugins/SiteManager/src/Controller/ArtifactsController.php
+++ plugins/SiteManager/src/Controller/ArtifactsController.php
@@ -5,7 +5,7 @@
 
 	class ArtifactsController extends AppController
 	{
-	    public function edit($id = null) {
+	    public function editText($id = null) {
 	    	$this->viewBuilder()->layout('ajax');
 			if(!is_numeric($id)) {
 				$artifact = $this->Artifacts->newEntity();
@@ -18,6 +18,7 @@
 		            $this->Flash->error(__('Unable to add your artifact.'));
 		        }
 				$artifact->name = $id; // THE ID IS THE NAME OF THE ARTIFACT THAT WAS CALLED
+				$artifact->type = 'text';
 			} else {
 				$artifact = $this->Artifacts->get($id);
 			    if ($this->request->is(['post', 'put'])) {
@@ -29,7 +30,34 @@
 			        $this->Flash->error(__('Unable to update your artifact.'));
 			    }
 			}
-			$this->set('options', ['text' => 'Text', 'heading' => 'Heading', 'image' => 'Image']);
+			$this->set('artifact', $artifact);
+	    }
+
+		public function editImage($id = null) {
+	    	$this->viewBuilder()->layout('ajax');
+			if(!is_numeric($id)) {
+				$artifact = $this->Artifacts->newEntity();
+		        if ($this->request->is(['post', 'put'])) {
+		            $artifact = $this->Artifacts->patchEntity($artifact, $this->request->data);
+		            if ($this->Artifacts->save($artifact)) {
+		                $this->Flash->success(__('Your artifact has been saved.'));
+		                return $this->redirect($this->referer());
+		            }
+		            $this->Flash->error(__('Unable to add your artifact.'));
+		        }
+				$artifact->name = $id; // THE ID IS THE NAME OF THE ARTIFACT THAT WAS CALLED
+				$artifact->type = 'image';
+			} else {
+				$artifact = $this->Artifacts->get($id);
+			    if ($this->request->is(['post', 'put'])) {
+			        $this->Artifacts->patchEntity($artifact, $this->request->data);
+			        if ($this->Artifacts->save($artifact)) {
+			            $this->Flash->success(__('Your artifact has been updated.'));
+			            return $this->redirect($this->referer());
+			        }
+			        $this->Flash->error(__('Unable to update your artifact.'));
+			    }
+			}
 			$this->set('artifact', $artifact);
 	    }
 	}
--- plugins/SiteManager/src/Template/Element/artifact_text.ctp
+++ plugins/SiteManager/src/Template/Element/artifact_text.ctp
@@ -9,9 +9,6 @@
 	
 	if(empty($artifact->id)) $id = str_replace(' ', '_', $name);
 	else $id = $artifact->id;
-	
-	if(!isset($class)) $class = 'lead';
-	if(!isset($tag))   $tag = 'h1';
 ?>
 
 <?php 
@@ -19,24 +16,22 @@
 	else $this_mode = 'public';
 ?>
 
-<div class="artifact-<?= $this_mode ?>">
+<span class="artifact-<?= $this_mode ?>">
 	
 	<?php if(empty($artifact)): ?>
-		<p class="lead">Empty Artifact</p>
-	<?php elseif($artifact->type == 'text'): ?>
-		<?= $this->SiteManager->autoParagraph($this->Text->autoLink($artifact->content), $class); ?>
-	<?php elseif($artifact->type == 'heading'): ?>
-		<<?= $tag ?>><?= $artifact->content; ?></<?= $tag ?>>
-	<?php endif;?>
+		Empty Artifact
+	<?php else: ?>
+		<?= $this->SiteManager->autoParagraph($this->Text->autoLink($artifact->content)); ?>
+	<?php endif; ?>
 
-	<div class="edit-box">
+	<span class="edit-box">
 		<?= 
 			$this->element('SiteManager.Bootstrap/button', [
 				'title' => $this->element('SiteManager.Bootstrap/icon', ['icon' => 'pencil']), 
 				'class' => 'primary btn-sm edit-button', 
 				'type' => 'button',
-				'url' => 'updateModal(\'/sitemgr/artifacts/edit/'. $id .'\')'
+				'url' => 'updateModal(\'/sitemgr/artifacts/edit_text/'. $id .'\')'
 			]); 
 		?>
-	</div>
-</div>
+	</span>
+</span>
